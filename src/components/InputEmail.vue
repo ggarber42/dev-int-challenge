f9567ffc2fb356549760199a94c8fd98cfe04a97
ggarber42 <ggarber@protonmail.com>
2021-10-03 13:01:37 -0300

    feat: email input component

---
+++ src/components/InputEmail.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="input-email">
+    <input
+      type="text"
+      :value='emailValue'
+      @input="$emit('update:emailValue', $event.target.value)"
+      required
+    >
+    <small
+      class="error"
+      :style="[isEmailValid ? {'visibility':'hidden'} : {'visibility':'visible'}]"
+    >Digite um email válido</small>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputEmail',
+  props: {
+    emailValue: {
+      type: String,
+      required: true
+    },
+    isEmailValid: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
