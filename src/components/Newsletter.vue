7100b20b99c81c38a864e0c65cdaf41dc69435ca
ggarber42 <ggarber@protonmail.com>
2021-10-03 08:38:04 -0300

    add: form into newsletter

--- src/components/Newsletter.vue
+++ src/components/Newsletter.vue
@@ -4,29 +4,29 @@
       <h2>Compartilhe a novidade</h2>
       <p>Quer que seus amigos também ganhem a lista personalizada deles? Preencha agora!</p>
     </div>
-    <div class="main">
-      <div class="input-box">
-        <label for="name">Nome do seu amigo:</label>
-        <input
-          type="text"
-          required
-          v-model="friendsName"
-        >
+    <form @submit.prevent="submitData">
+      <div class="main">
+        <div class="input-box">
+          <label for="name">Nome do seu amigo:</label>
+          <input
+            type="text"
+            required
+            v-model="friendsName"
+          >
+        </div>
+        <div class="input-box">
+          <label for="name">E-mail dele</label>
+          <input
+            type="text"
+            required
+            v-model="email"
+          >
+        </div>
       </div>
-      <div class="input-box">
-        <label for="name">E-mail dele</label>
-        <input
-          type="text"
-          required
-          v-model="email"
-        >
+      <div class="submit">
+        <button>Enviar agora</button>
       </div>
-    </div>
-    <div class="footer">
-      <button
-        @click="submitData"
-      >Enviar agora</button>
-    </div>
+    </form>
   </section>
 </template>
 
